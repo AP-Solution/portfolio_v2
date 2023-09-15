4140284636efc484bae556cf57eec79c583b88bf
Vyacheslav <haunt.beta@gmail.com>
2023-09-16 01:47:46 +0300

    add some films and raw version of ecommerce section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Main } from './components/Main/Main';
 import { Header } from './components/Header/Header';
 import { Services } from './components/Services/Services';
-import movies from './data/movies.json';
+import { Movies } from './components/Movies/Movies';
 
 function App() {
   const [isMenuActive, setIsMenuActive] = useState(false);
-  const [visibleMovies, setVisibleMovies] = useState(movies);
-  const [searchValue, setSearchValue] = useState('');
+  const [fetchedProducts, setFetchedProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(products => setFetchedProducts(products))
+  }, []);
 
   return (
     <div className="App">
@@ -23,78 +28,34 @@ function App() {
           <Route path="/blog" element={<div className="test-field">Blog</div>} />
         </Routes>
       </Router>
-      <Services />
-      <section className="about">
-
-
-        <div className="about__image">
-
-        </div>
-
-        <div className="about__text">
-          <h2>Трохи про мене</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <section className="store">
+        <h2 className="store__title">Віртуальний інтернет-магазин</h2>
+        <div className="store__wrapper">
+          <ul className="store__list">
+            {fetchedProducts.map(product => (
+               <li className="store__item" key={product.id}>
+               <div className="store__item-image">
+                 <img src={product.image} alt={product.title} />
+               </div>
+               
+               <h3>{product.title}</h3>
+               <p>Ціна: ${product.price}</p>
+               <p>Опис: {product.description}</p>
+               <p>Категорія: {product.category}</p>
+               <p>Рейтинг: {product.rating.rate}</p>
+             </li>
+            ))}
+          </ul>
         </div>
       </section>
-
-      <section className="movies">
-        <select
-          className="movies__select"
-          onChange={(e) => {
-            switch (e.target.value) {
-              case 'name':
-                setVisibleMovies([...movies].sort((a, b) => a.title.localeCompare(b.title)))
-                break
-
-              case 'year':
-                setVisibleMovies([...movies].sort((a, b) => a.year - b.year))
-                break
-
-              case 'duration':
-                setVisibleMovies([...movies].sort((a, b) => a.duration - b.duration))
-                break
-
-              default:
-                setVisibleMovies(movies);
-            }
-          }}>
-          <option>За замовчуванням</option>
-          <option value="name">За назвою</option>
-          <option value="duration">За тривалістю</option>
-          <option value="year">За роком випуску</option>
-
-
-        </select>
-
-        <div className="search__wrapper">
-          <input
-            type="text"
-            placeholder="type here to search..."
-            className="search__input"
-            onChange={(e) => {
-              setVisibleMovies(movies.filter(movie => movie.title.includes(e.target.value)));
-            }}
-          />
+      <Services />
+      <Movies />
+      <section className="about">
+        <div className="about__image"></div>
+        <div className="about__text">
+          <h2>Трохи про мене</h2>
+          <p>Ваш текст тут...</p>
         </div>
-        <ul className="movies__wrapper">
-          {visibleMovies && visibleMovies.map((movie) => {
-            return (
-              <li className="movies__item" key={movie.title}>
-                <img
-                  src={movie.preview_image}
-                  alt={movie.title}
-                  className="movie__image"
-                  value={searchValue}
-                />
-                <h2 className="movie__title">{movie.title}</h2>
-                <p className="movie__year">Рік випуску: {movie.year}</p>
-                <p className="movie__genre">Жанр: {movie.genre.join(', ')}</p>
-                <p className="movie__duration">Тривалість: {movie.duration} хв</p>
-                <p className="movie__actors">Актори: {movie.actors.join(', ')}</p>
-              </li>
-            )
-          })}
-        </ul>
       </section>
     </div>
   );
---
+++ src/components/Movies/Movies.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './Movies.css';
+import movies from '../../data/movies.json';
+import searchIcon from '../../images/icon__search.svg';
+
+export const Movies = () => {
+  const [visibleMovies, setVisibleMovies] = useState(movies);
+  return (
+    <section className="movies">
+        <h2 className="movies__title">Підбірка фільмів</h2>
+        <select
+          className="movies__select"
+          onChange={(e) => {
+            switch (e.target.value) {
+              case 'name':
+                setVisibleMovies([...movies].sort((a, b) => a.title.localeCompare(b.title)))
+                break
+
+              case 'year':
+                setVisibleMovies([...movies].sort((a, b) => a.year - b.year))
+                break
+
+              case 'duration':
+                setVisibleMovies([...movies].sort((a, b) => a.duration - b.duration))
+                break
+
+              default:
+                setVisibleMovies(movies);
+            }
+          }}>
+          <option>За замовчуванням</option>
+          <option value="name">За назвою</option>
+          <option value="duration">За тривалістю</option>
+          <option value="year">За роком випуску</option>
+
+
+        </select>
+
+        <div className="search__wrapper">
+          <img
+            src={searchIcon}
+            alt="search__icon"
+            className="search__icon"
+          />
+          <input
+            type="text"
+            placeholder="Почніть вводити назву..."
+            className="search__input"
+            onChange={(e) => {
+              setVisibleMovies(movies.filter(movie => movie.title.toLowerCase().includes(e.target.value.toLowerCase())));
+            }}
+          />
+        </div>
+        <ul className="movies__wrapper">
+          {visibleMovies && visibleMovies.map((movie) => {
+            return (
+              <li className="movies__item" key={movie.title}>
+                <img
+                  src={movie.preview_image}
+                  alt={movie.title}
+                  className="movie__image"
+                />
+                <h2 className="movie__title">{movie.title}</h2>
+                <p className="movie__year">Рік випуску: {movie.year}</p>
+                <p className="movie__genre">Жанр: {movie.genre.join(', ')}</p>
+                <p className="movie__duration">Тривалість: {movie.duration} хв</p>
+                <p className="movie__actors">Актори: {movie.actors.join(', ')}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+  )
+}
