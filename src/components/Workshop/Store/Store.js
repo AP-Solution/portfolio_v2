b82c9f448e2a230f4ee16289b7f0c9f10ae190dc
Vyacheslav <haunt.beta@gmail.com>
2023-09-17 19:46:11 +0300

    add some changes to store section

--- src/components/Workshop/Store/Store.js
+++ src/components/Workshop/Store/Store.js
@@ -3,26 +3,25 @@ import './Store.css';
 import { StoreItem } from './StoreItem';
 
 export const Store = () => {
-
   const [fetchedProducts, setFetchedProducts] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
 
   const updateTotalPrice = () => {
     const cartObject = JSON.parse(localStorage.getItem('cartObject')) || {};
     let total = 0;
-  
+
     for (const productId in cartObject) {
       const product = fetchedProducts.find((p) => p.id === parseInt(productId));
-      
+
       if (product) {
         total += product.price * cartObject[productId];
       }
     }
-  
+
     setTotalPrice(total);
   };
 
-  useEffect(() => {
+  const updateTotalPriceFromStorage = () => {
     const cartFromStorage = JSON.parse(localStorage.getItem('cartObject'));
     let total = 0;
 
@@ -33,32 +32,34 @@ export const Store = () => {
       }
     }
 
-    setTotalPrice(total)
-  }, [fetchedProducts])
+    setTotalPrice(total);
+  };
 
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
-      .then(res => res.json())
-      .then(products => setFetchedProducts(products))
+      .then((res) => res.json())
+      .then((products) => setFetchedProducts(products));
   }, []);
+
+  useEffect(() => {
+    updateTotalPriceFromStorage();
+  }, [fetchedProducts]);
 
   return (
     <section className="store">
       <h2 className="store__title">Віртуальний інтернет-магазин</h2>
       <div className="store__wrapper">
         <ul className="store__list">
-          {fetchedProducts.map(product => (
+          {fetchedProducts.map((product) => (
             <StoreItem
               product={product}
-              totalPrice={totalPrice}
-              setTotalPrice={setTotalPrice}
               updateTotalPrice={updateTotalPrice}
               key={product.id}
             />
           ))}
         </ul>
-        <div>Загалом до сплати: {totalPrice.toFixed(2)}</div>
+        <div>Загалом до сплати: {totalPrice.toFixed(2)} ₴</div>
       </div>
     </section>
-  )
+  );
 }
